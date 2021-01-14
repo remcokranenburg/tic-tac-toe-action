bdb0705364d61b261e2c2ce508811b66ff68d582
Remco Kranenburg <remco@burgsoft.nl>
2021-01-14 13:53:20 +0100

    Improve move handling code

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello, player! I'm going to win!");
+import * as core from @actions/core;
+import * as github from @actions/github;
+
+try {
+  console.log("Hello, player! I predict your move is going to be A1!");
+  const hardcodedMove = core.getInput("move");
+  console.log(`Your move is: ${hardcodedMove}`);
+
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+
+  console.log("Or is your move different?");
+  console.log(payload);
+} catch(error) {
+  core.setFailed(error.message);
+}
+
