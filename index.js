78abeaf7af435eb60f1afd24e0d1db0cf66d00f1
Remco Kranenburg <remco@burgsoft.nl>
2021-02-18 18:01:37 +0100

    Move posting functionality to Poster class

--- index.js
+++ index.js
@@ -5,18 +5,28 @@ import { Game } from "./tictactoe.js";
 
 const MOVE_COMMENT_PATTERN = /[A-C][1-3]/;
 
-async function post(owner, repo, issuNumber, comment) {
-  return await octokit.issues.createComment({
-      owner: owner,
-      repo: repo,
-      issue_number: issueNumber,
-      body: comment,
-  });
+class Poster {
+  constructor(octokit, owner, repo, issueNumber) {
+    this.octokit = octokit;
+    this.owner = owner;
+    this.repo = repo;
+    this.issueNumber = issueNumber;
+  }
+
+  post(comment) {
+    return this.octokit.issues.createComment({
+        owner: this.owner,
+        repo: this.repo,
+        issue_number: this.issueNumber,
+        body: comment,
+    });
+  }
 }
 
 async function run() {
   try {
     const token = core.getInput("token");
+    const octokit = github.getOctokit(token);
 
     const payload = github.context.payload;
     const body = payload.comment.body;
@@ -24,10 +34,11 @@ async function run() {
     const repo = payload.repository.name;
     const issueNumber = payload.issue.number;
 
+    const poster = new Poster(octokit, owner, repo, issueNumber);
+
     if(body.match(MOVE_COMMENT_PATTERN)) {
       let response = "";
 
-      const octokit = github.getOctokit(token);
       const commentsRequest = await octokit.request(
           "GET /repos/{owner}/{repo}/issues/{issue_number}/comments", {
             owner: owner,
@@ -47,17 +58,17 @@ async function run() {
       moves.forEach((c) => game.makeMove(c.body));
 
       if(game.isGameOver()) {
-        await post(owner, repo, issueNumber, "Error: game is already over!");
+        await poster.post("Error: game is already over!");
       } else {
         const counterMove = game.calculateBestMove();
         game.makeMove(counterMove);
-        await post(owner, repo, issueNumber, counterMove);
+        await poster.post(counterMove);
 
         if(game.isGameOver()) {
-          await post(owner, repo, issueNumber, "Game over!");
+          await poster.post("Game over!");
         }
 
-        await post(owner, repo, issueNumber, game.toString());
+        await poster.post(game.toString());
       }
     }
   } catch(error) {
