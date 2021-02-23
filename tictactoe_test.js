e028cf1e8096b9e66964e826e5c92f75a6f6e66a
Remco Kranenburg <remco@burgsoft.nl>
2021-02-23 18:11:05 +0100

    Improve tests

--- tictactoe_test.js
+++ tictactoe_test.js
@@ -14,18 +14,27 @@ function test_empty() {
 
 function test_make_move() {
   const expectedBoard = [
-      [ "X", null,  "O"],
-      [null,  "X", null],
-      [null, null, null],
+      [ "X", null, "O"],
+      [null,  "X", "O"],
+      [null, null, "X"],
   ];
   const game = new Game();
   game.makeMove("B2");
   game.makeMove("C3");
   game.makeMove("A3");
+  game.makeMove("C2");
+  game.makeMove("C1");
   assert.deepEqual(game.board, expectedBoard);
+  assert.equal(game.isGameOver(), true);
+  assert.equal(game.hasWinner(), true);
 }
 
-function test_draw() {
+function test_make_move_draw() {
+  const expectedBoard = [
+      ["O", "X", "X"],
+      ["X", "O", "O"],
+      ["X", "O", "X"],
+  ];
   const game = new Game();
   game.makeMove("A1");
   game.makeMove("B2");
@@ -36,9 +45,11 @@ function test_draw() {
   game.makeMove("B3");
   game.makeMove("C2");
   game.makeMove("C3");
+  assert.deepEqual(game.board, expectedBoard);
   assert.equal(game.isGameOver(), true);
+  assert.equal(game.hasWinner(), false);
 }
 
 test_empty();
 test_make_move();
-test_draw();
+test_make_move_draw();
